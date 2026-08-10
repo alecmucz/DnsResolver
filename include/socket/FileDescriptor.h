eb503c85d4e643750835585e9aab597c58dcea65
Alec <alecmucz@gmail.com>
2026-08-09 21:30:26 -0400

    File Descriptor Abstraction (Rule of 5)

--- include/socket/FileDescriptor.h
+++ include/socket/FileDescriptor.h
@@ -1,8 +1,36 @@
 #pragma once
+#include <unistd.h>
 
 class FileDescriptor {
 private:
     int fd{-1};
 
 public:
+    FileDescriptor() = default;
+    explicit FileDescriptor(int fd) : fd(fd) {}
+
+    FileDescriptor(const FileDescriptor&) = delete;             // Copy Constructor (Not Allowed)
+    FileDescriptor& operator=(const FileDescriptor&) = delete;  // Copy Assignment Operator (Not Allowed)
+
+    FileDescriptor(FileDescriptor&& other) noexcept : fd(other.fd)  // Move Constructor
+    {other.fd = -1;}
+
+    FileDescriptor& operator=(FileDescriptor&& other) noexcept {    // Move Assignment Operator
+        if (fd != -1)
+            close(fd);
+
+        fd = other.fd;
+        other.fd = -1;
+        return *this;
+    }
+
+
+    ~FileDescriptor() {     // Destructor (Close the file descriptor)
+        if (fd != -1)
+            close(fd);
+    }
+
+    int value() const {
+        return fd;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <utility>
+#include "include/socket/FileDescriptor.h"
 
 int main() {
-    std::print("DNS Resolver Project");
+    FileDescriptor fd_a{4};
+    FileDescriptor fd_b{5};
+
+    std::print("Before move:\n");
+    std::print("fd_a = {}\n", fd_a.value());
+    std::print("fd_b = {}\n", fd_b.value());
+
+    fd_a = std::move(fd_b);
+
+    std::print("After move:\n");
+    std::print("fd_a = {}\n", fd_a.value());
+    std::print("fd_b = {}\n", fd_b.value());
     return 0;
 }
